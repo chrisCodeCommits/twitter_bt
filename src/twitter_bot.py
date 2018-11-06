7b25afaf46049ef7d814af310aa8f410da7d29f1
chris <chrispyprogrammer@gmail.com>
2018-11-06 20:15:51 +0200

    not handling errors correctly/ needs few adjustments

--- src/twitter_bot.py
+++ src/twitter_bot.py
@@ -28,6 +28,9 @@ twitter_api = tweepy.API(auth)
 
 
 
+hashtag = '#tstapp2018twmat'
+
+
 
 ## EXTRACTING AND PROCESSING DATA #####################################################
 
@@ -46,36 +49,78 @@ class HashtagListner(StreamListener):
         tweet            = tweet_detials['text']
         tweet_id         = tweet_detials['id']
         user_to_tweet_to = tweet_detials['user']['screen_name']
+        tweet_format     = tweet.split(" ")
 
 
-        #Extracting the DOI from tweets
-        tweeted_doi = tweet.split(" ")[1]
+        print(len(tweet_format))
+        print('code 1 executed')
 
-        # Connecting to Unpaywall API and passing the extracted DOI
-        # in order to find a match
-        response = requests.get(
-            f'http://api.unpaywall.org/{tweeted_doi}?email=chrispyprogrammer@gmail.com'
-            )
+        # Checking the tweet format befor processing the request
+        if len(tweet_format) == 3 and tweet_format[0].lower() == 'doi:' and tweet_format[-1].lower() == hashtag:
 
-        json_data = response.json()
+            #Extracting the DOI from the tweet
+            tweeted_doi = tweet_format[1]
 
-        # Extracting the link to the document
-        free_fulltext_url = json_data['results'][0]['free_fulltext_url']
+            print('code 2 executed')
+
+            # Connecting to Unpaywall API and passing the extracted DOI
+            # in order to find a match
+            response = requests.get(
+                f'http://api.unpaywall.org/{tweeted_doi}?email=chrispyprogrammer@gmail.com'
+                )
+
+            print('code 3 executed')
+
+            # To handle cases where the DOI is not found or misspelled
+            if response.reason == 'NOT FOUND':
+
+                not_found_message = (
+
+                f"@{user_to_tweet_to} I'm a bot who uses the @unpaywall API to look for"\
+                "free, legal full text documents. I didn't found anything in the data base"\
+                f"corresponding to the DOI you provided --> {tweeted_doi}"
+
+                    )
+
+                print('code 4 executed')
 
 
-        my_reply = (
-
-        f"@{user_to_tweet_to} I'm a bot who uses the @unpaywall API to look for"\
-        "free, legal full text documents. I found a free copy of what you requested"\
-        f"here: {free_fulltext_url}"
-
-        )
+                twitter_api.update_status(not_found_message, in_reply_to_status_id = tweet_id)
 
 
-        # Tweeting the result to the person who submited the request
-        twitter_api.update_status(my_reply, in_reply_to_status_id = tweet_id)
+            else:
 
-        print('code executed')
+                json_data = response.json()
+
+                # Extracting the link to the document
+                free_fulltext_url = json_data['results'][0]['free_fulltext_url']
+
+                my_reply = (
+
+                f"@{user_to_tweet_to} I'm a bot who uses the @unpaywall API to look for"\
+                "free, legal full text documents. I found a free copy of what you requested"\
+                f"here: {free_fulltext_url}"
+
+                )
+
+                # Tweeting the result to the person who submited the request
+                twitter_api.update_status(my_reply, in_reply_to_status_id = tweet_id)
+
+                print('code 5 executed')
+
+
+        else:
+
+            wrong_format_message = (
+
+            f"@{user_to_tweet_to} I'm a bot who uses the @unpaywall API to look for"\
+            "free, legal full text documents. I can find what you're looking for"\
+            f"if you format your tweet this way -->  DOI: your_doi_here {hashtag}"
+
+                )
+
+            twitter_api.update_status(wrong_format_message, in_reply_to_status_id = tweet_id)
+
 
         return True
 
@@ -87,9 +132,6 @@ class HashtagListner(StreamListener):
 
 
 ## TRACKING THE HASHTAG ###############################################################
-
-
-hashtag = '#tstapp2018twmat'
 
 
 if __name__ == '__main__':
